54baecd8bcdbf8339052b0b19b1875ae9977f2c1
awangelo <anggg.gaio@gmail.com>
2024-08-14 09:48:41 -0300

    comentarios sobre casos

--- mysql/main.go
+++ mysql/main.go
@@ -18,6 +18,8 @@ func main() {
 	// 	Addr:   "127.0.0.1:3306",
 	// 	DBName: "recordings",
 	// }
+	// `db` eh um objeto que representa a conexao com o banco de dados,
+	// ele possui metodos para executar queries e transacoes.
 	db, err := sql.Open("mysql", "root:@(127.0.0.1:3306)/coisas?parseTime=true")
 	if err != nil {
 		log.Fatal(err)
@@ -32,7 +34,7 @@ func main() {
 	// Query para ser executada.
 	query := "SELECT * FROM blahaj;"
 
-	// Executa a query e guarda o resultado em rows.
+	// Executa uma query e guarda o resultado em rows.
 	rows, err := db.Query(query)
 	if err != nil {
 		log.Fatal(err)
@@ -58,4 +60,35 @@ func main() {
 	if err = rows.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	// Query de multiplas linhas pode usar uma struct e slices.
+	// type Album struct {
+	// 	ID     int64
+	// 	Title  string
+	// 	Artist string
+	// 	Price  float32
+	// }
+	// var albums []Album
+	// ...
+	// Perceba que no codigo acima foi usado var id, var name, var color...
+	// Agora os dados serao escritos em uma struct, como abaixo: &alb.ID, &alb.Title.
+	// for rows.Next() {
+	//     var alb Album
+	//     if err := rows.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+	//         return nil, fmt.Errorf("albumsByArtist %q: %v", name, err)
+	//     }
+	// 	   albums = append(albums, alb)
+	// }
+
+	// Query de unica linha
+	// var alb Album
+	// `db.QueryRow` espera por uma query que retorne apenas uma row.
+	// row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+	// if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+	//     if err == sql.ErrNoRows {
+	//         return alb, fmt.Errorf("albumsById %d: no such album", id)
+	//     }
+	//     return alb, fmt.Errorf("albumsById %d: %v", id, err)
+	// }
+	// return alb, nil
 }
